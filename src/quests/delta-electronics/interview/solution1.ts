78065c0332be26110ca40106a4b8d8147d2ef76c
ren-joey <tsuyiren@gmail.com>
2024-10-13 01:15:18 +0800

    docs: 📋 add some comments

--- src/quests/delta-electronics/interview/solution1.ts
+++ src/quests/delta-electronics/interview/solution1.ts
@@ -1,13 +1,18 @@
 
 export const maxSubArray = (input: number[]): number[] => {
+    // Use array to store dynamic programming results from each index,
+    // and another array to store the length of the corresponding index.
     const dp1 = new Array(input.length).fill(-Infinity);
     const strLen1 = new Array(input.length).fill(-1);
     const dp2 = new Array(input.length).fill(-Infinity);
     const strLen2 = new Array(input.length).fill(-1);
 
+    // Record the optimal result, its index, and its length.
     let max = input[0];
     let maxLen = 1;
     let maxIdx = 0;
+
+    // Initialize the first element as the first element of the input array.
     dp1[0] = input[0];
     strLen1[0] = 1;
     dp2[0] = input[0];
@@ -15,6 +20,8 @@ export const maxSubArray = (input: number[]): number[] => {
 
     for (let i = 1; i < input.length; i += 1) {
         const target = input[i];
+
+        // Compare the current element with the sum of itself and previous dp1 result.
         if (target > target + dp1[i-1]) {
             dp1[i] = target;
             strLen1[i] = 1;
@@ -33,6 +40,7 @@ export const maxSubArray = (input: number[]): number[] => {
             }
         }
 
+        // Compare the dp1 result with the previous dp2 result.
         if (dp1[i] > dp2[i-1]) {
             dp2[i] = dp1[i];
             strLen2[i] = strLen1[i];
